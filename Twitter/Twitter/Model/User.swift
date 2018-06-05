0a2c2de5d163868fae2094ef7f8c98a2e3d404af
Anthony Hoang <anthony.hoang@slalom.com>
2018-06-05 13:05:13 -0500

    adding pods. setting up directory structure. adding data models

---
+++ Twitter/Twitter/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by Anthony Hoang on 6/5/18.
+//  Copyright © 2018 Anthony Hoang. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct User {
+    let id: String
+}
